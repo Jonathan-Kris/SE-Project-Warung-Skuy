d8331c3ae69c7aaf248c2a53db7a5545b760faa0
Jonathan-Kris <52554124+Jonathan-Kris@users.noreply.github.com>
2021-06-02 14:07:29 +0700

    feat: Add validation in pengajuan pinjaman form & create S&K field

--- warungskuy/forms.py
+++ warungskuy/forms.py
@@ -1,8 +1,9 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, SubmitField, RadioField, SelectField, DecimalField, IntegerField, FloatField, TextAreaField, TextField
-from wtforms.validators import Length, EqualTo, Email, DataRequired, ValidationError
 from wtforms.fields.html5 import DateField
+from wtforms.validators import DataRequired, Email, EqualTo, InputRequired, Length, NumberRange, ValidationError
 from warungskuy.models import User, Lender, Borrower
+from wtforms.fields.simple import BooleanField
 
 
 class RegisterLenderForm(FlaskForm):
@@ -46,7 +47,8 @@ class RegisterLenderForm(FlaskForm):
         DataRequired()])
     gender = RadioField(label='Jenis Kelamin', choices=['Laki-Laki', 'Perempuan'], validators=[
         DataRequired()])
-    bank = SelectField(u'Bank', choices=[('bbca', 'Bank Central Asia'),
+    bank = SelectField(u'Bank', choices=[('XXXXXX', '-- Pilih Bank --'),
+                                         ('bbca', 'Bank Central Asia'),
                                          ('bmri', 'Bank Mandiri'),
                                          ('bbri', 'Bank Rakyat Indonesia')],
                        validators=[DataRequired()])
@@ -54,6 +56,8 @@ class RegisterLenderForm(FlaskForm):
         label='Nomor Rekening', validators=[DataRequired()])
     account_name = StringField(
         label='Nama Pemilik Rekening', validators=[DataRequired()])
+
+    toc = BooleanField(label="Setuju dengan syarat dan ketentuan yang berlaku", validators=[InputRequired()])
     submit = SubmitField(label='Daftar')
 
 
@@ -91,6 +95,7 @@ class RegisterBorrowerForm(FlaskForm):
         DataRequired()])
     gender = RadioField(label='Jenis Kelamin', choices=['Laki-Laki', 'Perempuan'], validators=[
         DataRequired()])
+    toc = BooleanField(label="Setuju dengan syarat dan ketentuan yang berlaku", validators=[InputRequired()])
     submit = SubmitField(label='Daftar')
 
 
@@ -101,21 +106,24 @@ class LoginForm(FlaskForm):
 
 
 class LoanForm(FlaskForm):
-    title = StringField(label="Judul Pinjaman", validators=[DataRequired()])
-    tenor = FloatField(label="Tenor", validators=[])
-    start_loan = DateField(label="Mulai Pinjaman", validators=[])
-    end_loan = DateField(label="Batas Akhir Pinjaman", validators=[])
-    nominal = IntegerField(label="Nominal Pinjaman", validators=[])
-    interest = FloatField(label="Bunga Efektif", validators=[])
-    loan_reason = TextAreaField(label="Alasan Pinjaman", validators=[])
-    business_desc = TextAreaField(label="Keterangan Usaha", validators=[])
-    location = StringField(label="Domisili", validators=[])
-    start_year = StringField(label="Tahun Rintis", validators=[])
+    title = StringField(label="Judul Pinjaman", validators=[InputRequired()])
+    tenor = IntegerField(label="Tenor (bulan)", validators=[InputRequired()])
+    start_loan = DateField(label="Mulai Pinjaman", validators=[InputRequired()])
+    end_loan = DateField(label="Batas Akhir Pinjaman", validators=[InputRequired()])
+    nominal = IntegerField(label="Nominal Pinjaman", validators=[InputRequired(), NumberRange(min=0)])
+    interest = FloatField(label="Bunga Efektif", validators=[InputRequired(), 
+                        NumberRange(min=0.0, max=100.0, message="Nilai di antara 0-100 %")])
+    loan_reason = TextAreaField(label="Alasan Pinjaman", validators=[InputRequired()])
+    business_desc = TextAreaField(label="Keterangan Usaha", validators=[InputRequired()])
+    location = StringField(label="Domisili", validators=[InputRequired()])
+    start_year = IntegerField(label="Tahun Rintis", validators=[InputRequired(), NumberRange(min=1970)])
     business_address = StringField(
-        label="Alamat atau Lokasi Usaha", validators=[])
+        label="Alamat atau Lokasi Usaha", validators=[InputRequired()])
     gross_income = IntegerField(
-        label="Pendapatan Kotor per Bulan", validators=[])
+        label="Pendapatan Kotor per Bulan", validators=[InputRequired(), NumberRange(min=0)])
     net_income = IntegerField(
-        label="Pendapatan Bersih per Bulan", validators=[])
-    modal = IntegerField(label="Modal Usaha", validators=[])
+        label="Pendapatan Bersih per Bulan", validators=[InputRequired(), NumberRange(min=0)])
+    modal = IntegerField(label="Modal Usaha", validators=[InputRequired(), NumberRange(min=0)])
+
+    toc = BooleanField(label="Setuju dengan syarat dan ketentuan yang berlaku", validators=[InputRequired()])
     submit = SubmitField(label="Proses Peminjaman")
