7cd0bcc0791376c2fbbc63dbc042496bec364337
AsterinGray <raymondtiandinata@gmail.com>
2021-06-09 10:41:49 +0700

    Register bug fixed

--- warungskuy/static/js/pages/auth.js
+++ warungskuy/static/js/pages/auth.js
@@ -4,13 +4,34 @@ const changeForm = () => {
   const form_investor = $('#investor-form');
   const form_peminjam = $('#peminjam-form');
 
-  if (type === 'Investor') {
-    $('#type').text('Peminjam');
+  if (type === 'Lender') {
+    $('#type').text('Borrower');
     form_investor.hide();
     form_peminjam.show();
+    localStorage.setItem('register', 'borrower');
+    console.log(localStorage.getItem('register'));
   } else {
-    $('#type').text('Investor');
+    $('#type').text('Lender');
     form_investor.show();
     form_peminjam.hide();
+    localStorage.setItem('register', 'lender');
+    console.log(localStorage.getItem('register'));
   }
 };
+
+$(() => {
+  const type = $('#type').text();
+
+  const form_investor = $('#investor-form');
+  const form_peminjam = $('#peminjam-form');
+
+  if (localStorage.getItem('register') == 'lender') {
+    $('#type').text('Lender');
+    form_investor.show();
+    form_peminjam.hide();
+  } else if (localStorage.getItem('register') == 'borrower') {
+    $('#type').text('Borrower');
+    form_investor.hide();
+    form_peminjam.show();
+  }
+});
