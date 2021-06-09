631c0fa96b9d8722bf11145761119f35433c61c8
AsterinGray <raymondtiandinata@gmail.com>
2021-06-09 14:31:55 +0700

    merge main page pendanaan dan peminjaman from FE to BE

--- warungskuy/static/js/pages/auth.js
+++ warungskuy/static/js/pages/auth.js
@@ -9,19 +9,15 @@ const changeForm = () => {
     form_investor.hide();
     form_peminjam.show();
     localStorage.setItem('register', 'borrower');
-    console.log(localStorage.getItem('register'));
   } else {
     $('#type').text('Lender');
     form_investor.show();
     form_peminjam.hide();
     localStorage.setItem('register', 'lender');
-    console.log(localStorage.getItem('register'));
   }
 };
 
 $(() => {
-  const type = $('#type').text();
-
   const form_investor = $('#investor-form');
   const form_peminjam = $('#peminjam-form');
 
